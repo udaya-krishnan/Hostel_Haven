1efce5970bfa01647be8135592a3a7ae42d6bf6a
udaya-krishnan <udayankrishnan36@gmail.com>
2024-10-29 17:43:14 +0530

    ..

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -23,7 +23,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
 app.use(cors({
-    origin:'https://hostel-haven.vercel.app/',
+    origin:'https://hostel-haven.vercel.app',
     credentials:true
 }))
 app.use(cookieSession({
